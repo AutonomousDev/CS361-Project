dbb1c106d0bfc677904b13614e827fcb544b10a7
Cameron Bowers <cameronbowers0@gmail.com>
2022-07-04 18:48:56 -0700

    Added bootstrap and a nav bar

--- review_board/products/models.py
+++ review_board/products/models.py
@@ -9,3 +9,10 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('product-detail', kwargs={'pk': self.pk})
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    comments = models.TextField()
+
